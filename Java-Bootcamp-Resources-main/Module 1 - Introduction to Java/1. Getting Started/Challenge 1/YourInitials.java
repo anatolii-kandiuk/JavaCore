4c30ecf0daad133d0527ae709190dae2fa3f18f3
anatolii <batkovich327@gmail.com>
2026-03-25 22:28:18 +0100

    Add YourInitials class to print initials in a formatted manner

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,11 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("K   K    A  ");
+        System.out.println("K  K    A A ");
+        System.out.println("K K    A   A");
+        System.out.println("KK     AAAAA");
+        System.out.println("K K    A   A");
+        System.out.println("K  K   A   A");
+        System.out.println("K   K  A   A");
     }
 }
