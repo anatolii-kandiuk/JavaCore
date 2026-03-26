406b8f066376551cc3261d974b614ef729398e64
anatolii <batkovich327@gmail.com>
2026-03-26 22:25:39 +0100

    Add Variables class to demonstrate variable declaration and output in Java

---
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 7;
+        int busTickets = 3;
+
+        System.out.println(myFavoriteNumber);
+        System.out.println(busTickets);
+    }
+}
