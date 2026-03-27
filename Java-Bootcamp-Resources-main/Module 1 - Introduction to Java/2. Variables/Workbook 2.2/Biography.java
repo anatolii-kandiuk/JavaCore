d2cb04d8fc5b5db3aa3db734aa556b377bba1c3c
anatolii <batkovich327@gmail.com>
2026-03-28 00:25:13 +0100

    Refactor Biography class to initialize variables and update output statements for clarity

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -3,20 +3,27 @@ public class Biography {
         
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.2 to access the link).
 
-
         // name (text)
+        String myName = "Anatolii";
         // age (whole number)
+        int myAge = 30;
         // country (text)
+        String myCountry = "Ukraine";
         // sport (text)
+        String mySport = "football";
         // hours (whole number)
+        int myHours = 3;
         // game (text)
+        String myGame = "Assassin's Creed";
         // subject (text)
+        String mySubject = "Mathematics";
         // grade (character)
+        char myGrade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + myName + ". I'm " + myAge + " years old, and I'm from " + myCountry + ".");
+        System.out.println("My favourite sport is " + mySport + ". I play for " + myHours + " hours a day.");
+        System.out.println("When I'm tired, I like to play " + myGame + ".");
+        System.out.println("In school, my favourite subject was " + mySubject + ", I scored a " + myGrade + ".");
 
         // Compare your result to what's on Learn the Part.
     }
