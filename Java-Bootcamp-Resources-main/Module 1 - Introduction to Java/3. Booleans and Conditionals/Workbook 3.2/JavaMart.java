59ab002b3eca527b98fe3274503a225fab78ef72
anatolii <batkovich327@gmail.com>
2026-03-31 23:02:21 +0200

    Fix output statements in JavaMart class to correctly display wallet balance after purchases

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.2/JavaMart.java
@@ -14,14 +14,28 @@ public class JavaMart {
 
         //else:  Sorry, I only have <wallet> left.
 
+        if (wallet >= toyCar) {
+            System.out.println("Sure!");
+            wallet -= toyCar;
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
         
         double nike = 95.99;
         System.out.println("Can I get these nike shoes?");
+        
         //if you have enough money
             //       print: Sure!
             //       pay for the nike shoes 
 
         //else: Sorry, I only have <wallet> left.
+
+        if (wallet >= nike) {
+            System.out.println("Sure!");
+            wallet -= nike;
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
 
     }
 }
