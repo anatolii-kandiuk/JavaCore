c2463e5b20e27ed9dd76396ee35743618a96c0f7
anatolii <batkovich327@gmail.com>
2026-04-09 22:18:37 +0200

    Refactor Bank class to enhance user input handling and improve mortgage approval logic

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
@@ -6,31 +6,52 @@ public class Bank {
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
         Scanner scan = new Scanner(System.in);
+        double savings = 0;
+        double debt = 0;
+        int years = 0;
+        String name = "";
+        
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
         //Task 1 - Pick up the user's decision.
-
+        String decision = scan.nextLine();
 
         //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
-            "\nHow much money do you have in your savings?" +
-            "\nAnd, how much do you owe in credit card debt?");
+        switch (decision) {
+            case "yes":
                 // Task 3 - Pick up each value 
-            System.out.println("\nHow many years have you worked for?");
-               // Task 4 - Pick up number of years
-
-            System.out.println("What is your name?");
-               // Task 5 - Pick up the user's name
+                System.out.println("\nGreat! In one line" +
+                                    "\nHow much money do you have in your savings?" +
+                                    "\nAnd, how much do you owe in credit card debt?");
+                savings = scan.nextDouble();
+                debt = scan.nextDouble();
+                scan.nextLine(); // Consume the newline character
                 
-               //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
+                // Task 4 - Pick up number of years
+                System.out.println("\nHow many years have you worked for?");
+                years = scan.nextInt();
+                scan.nextLine();
 
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
-
-
+                // Task 5 - Pick up the user's name
+                System.out.println("What is your name?");
+                name = scan.nextLine();
+                
+                break;
+            case "no":
+                //Task 2 - Print this if the decision was not "yes"
+                System.out.println("\nOK. Have a nice day!");
+                break;
+            default:
+                System.out.println("\nThat's not a valid answer.");
+                break;
+        }
+        //Task 6 - Approve the mortgage if they meet the requirements (see article)
+        //       - Otherwise, don't give them a mortgage.     
+        if (savings >= 10000 && debt <= 5000 && years >= 2) {
+            System.out.println("\nCongratulations " + name + " You have been approved!");
+        } else {
+            System.out.println("\nSorry, you are not eligible for a mortgage.");
+        }
         scan.close();
     }
 }
