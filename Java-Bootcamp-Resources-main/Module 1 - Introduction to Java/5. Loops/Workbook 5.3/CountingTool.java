834160808ad1584904d9940dea45f6f8c85be183
anatolii <batkovich327@gmail.com>
2026-04-12 23:13:32 +0200

    Refactor CountingTool class to improve user interaction and counting functionality

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -5,7 +5,16 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
+        System.out.print("\nHi Timmy. What number do you want to count to ?  ");
 
+        int number = scan.nextInt();
+
+        for (int i = 0; i <= number; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n");
+        
         scan.close();
     }
 }
