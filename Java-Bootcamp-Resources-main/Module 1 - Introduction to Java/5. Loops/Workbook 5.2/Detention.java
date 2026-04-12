579ade40457c5a532770643b5f83c2cbfe8674b0
anatolii <batkovich327@gmail.com>
2026-04-12 23:06:35 +0200

    Add user input functionality to Detention class for custom line writing

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
 
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
         // See Learn the Part for the instructions.
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + ". " + line);
+        }
         
     }
 }
