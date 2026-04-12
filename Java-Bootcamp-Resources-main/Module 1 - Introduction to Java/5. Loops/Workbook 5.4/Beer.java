9aa5d7fce511835c2867d518323f84dfe1b1982a
anatolii <batkovich327@gmail.com>
2026-04-12 23:20:43 +0200

    Add Beer class with sing method to print lyrics of the song

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.4/Beer.java
@@ -1,6 +1,23 @@
 public class Beer {
     public static void main(String[] args) {
        // See detailed instructions on Learn the Part.
+       sing(100);
     }
 
+    /**
+     * Function name -- sing. <------
+     * @param i (int)
+     *
+     *
+     * Inside the function:
+     *  1. print:
+     *    <i> bottles of beer on the wall, <i> bottles of beer!
+     *    take one down, pass it around <i-1>, bottles of beer on the wall!
+     */
+    private static void sing(int i) {
+        for (int j = i; j > 0; j--) {
+            System.out.println(j + " bottles of beer on the wall, " + j + " bottles of beer!");
+            System.out.println("Take one down, pass it around, " + (j - 1) + " bottles of beer on the wall!");
+        }
+    }
 }
