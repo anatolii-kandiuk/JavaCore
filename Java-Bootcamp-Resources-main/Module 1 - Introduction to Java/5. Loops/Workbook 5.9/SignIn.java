ed2888d66b0e36fdeb827556c31e3c231cdada2c
anatolii <batkovich327@gmail.com>
2026-04-15 17:08:09 +0200

    Refactor SignIn class to streamline user input process and enhance readability

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.9/SignIn.java
@@ -5,30 +5,25 @@ public class SignIn {
         String username = "Samantha";
         String password = "Java <3";
         
-        /* Task 1 
-            1. Pick up a username and password from the user.
-       */
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
-        System.out.print("- Username: ");
-        //pick up username
-        System.out.print("- Password: ");
-        //pick up password
+       
+        System.out.print("- Username: "); 
+        String usernameEntry = scan.nextLine();
 
-        // see Learn the Part for the remaining instructions.
-        
-        
-        /* will be useful for task 2
-        
+        System.out.print("- Password: ");
+        String passwordEntry = scan.nextLine();
+
+        while (!username.equals(usernameEntry) || !password.equals(passwordEntry) ) {
             System.out.println("\nIncorrect, please try again!\n");
+            
             System.out.print("- Username: ");
             usernameEntry = scan.nextLine();
             System.out.print("- Password: ");
             passwordEntry = scan.nextLine();
+        }
+        System.out.println("Sign in successful. Welcome!");
 
-        
-        */
         scan.close();
-        
     }
 }
